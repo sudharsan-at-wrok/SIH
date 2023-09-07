bafb19b8f5bd22dc6cdd893acbf31cae7b948c66
sudharsan-15 <sudharsans21ec@psnacet.edu.in>
2023-09-07 15:05:51 +0530

    homepage update

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      home: const HomePage(),
+      theme: ThemeData(fontFamily: 'Araboto'),
       debugShowCheckedModeBanner: false,
     );
   }
